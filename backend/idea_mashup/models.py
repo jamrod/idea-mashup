299101b9bf93b0ffc0d84ea4264ee361156f757b
Jamrod <myfavoritenoisemaker@gmail.com>
2020-06-24 16:56:27 -0600

    working on serializations

--- backend/idea_mashup/models.py
+++ backend/idea_mashup/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Who(models.Model):
+    data = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.data
+
+
+class What(models.Model):
+    data = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.data
+
+
+class Where(models.Model):
+    data = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.data
+
+
+class Why(models.Model):
+    data = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.data
